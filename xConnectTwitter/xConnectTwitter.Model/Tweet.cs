e8f7eaa27d2c33c378f736b643127d5eb0effabc
Jason Wilkerson <jason.wilkerson@rbaconsulting.com>
2017-08-21 00:11:34 -0500

    adding custom model

---
+++ xConnectTwitter/xConnectTwitter.Model/Tweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.XConnect;
+
+namespace xConnectTwitter.Model
+{
+    public class Tweet : Facet
+    {
+        public const string FacetName = "Tweet";
+
+        public string Text { get; set; }
+    }
+}
---
+++ xConnectTwitter/xConnectTwitter.Model/TweetEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.XConnect;
+
+namespace xConnectTwitter.Model
+{
+    public class TweetEvent : Event
+    {
+        public TweetEvent(Guid definitionId, DateTime timestamp) : base(definitionId, timestamp)
+        {
+        }
+
+        public string TargetPhrase { get; set; }
+    }
+}
